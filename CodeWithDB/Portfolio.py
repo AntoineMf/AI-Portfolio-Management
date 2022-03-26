2efd80326f7ff1950048c397bec638d6263d6274
AurelienLeo <aurelien.leonard12@gmail.com>
2022-03-26 01:21:07 +0100

    debut commentaires

--- CodeWithDB/Portfolio.py
+++ CodeWithDB/Portfolio.py
@@ -5,17 +5,20 @@ import random as rd
 
 
 class Portfolio:
-    ''' Creation d'un portefeuille avec tout les parametre clients,
-    car dans cette classe on implémente aussi la fitness'''
-    # --- Constructor and Attributes --- #
-    def __init__(self, listOfAssets, amount, returnsClient, volClient, weights=0, score=0):
+    """
+    Creation d'un portefeuille avec tout les parametres demandes du clients et gestion de la fitness
+    """
+    def __init__(self, listOfAssets, amount, returnsClient, volClient, weights=0):
+        """
+        Constructeur du portefeuille, si les poids sont nuls de nouveaux sont generes
+        """
         self.returnsClient = returnsClient
         self.volClient = volClient
         if self.volClient != 0 and self.returnsClient != 0:
             self.sharpeClient = returnsClient/volClient
         self.listOfAssets = listOfAssets
         self.numberOfAssets = 6
-        self.score = score
+        self.score = 0
         self.amount = amount
         self.shares = []
         self.weights = weights if weights != 0 else self.RandomWeights()
@@ -28,20 +31,11 @@ class Portfolio:
         self.fitness()
     # --- Methods --- #
     # Sort a population based on..
-    """
-    def ChooseAssets(self, numberOfAssets=38):
-        temp = list()
-        index = range(0, len(self.listOfAssets.listAssets))
-        while len(temp) < numberOfAssets:
-            val = rd.choices(index)[0]
-            if val not in temp:
-                temp.append(val)
-        choices = [True if i in temp else False for i in range(0, len(self.listOfAssets.listAssets))]
-        return choices
-    """
 
-    '''??? Choisis des assets de manière aléatoire'''
     def ChooseAssets(self):
+        """
+        Selection d'actifs aleatoirement (a integrer dans le portefeuille)
+        """
         choices = list()
         index = range(0, len(self.listOfAssets.listAssets))
         while len(choices) < self.numberOfAssets:
@@ -49,129 +43,6 @@ class Portfolio:
             if val not in choices:
                 choices.append(val)
         return choices
-
-    """
-    def MaxShares(self, lastPrices, choices):
-        prices = [lastPrices[i] if choices[i] else float('inf') for i in range(0, len(lastPrices))]
-        maxShares = int(self.amount / (min(prices) * 2))
-        return maxShares
-    """
-    """
-    def RandomWeights(self, confidence=0.01, numberOfAssets=38):
-        lastPrices = self.listOfAssets.LastPrices()
-        choices = self.ChooseAssets(numberOfAssets)
-        maxShares = self.MaxShares(lastPrices, choices)
-        print(maxShares)
-        #print(choices)
-        print("First")
-        while True:
-            amountToBeFilled = self.amount
-            shares = [rd.randint(1, maxShares) if choices[i] else 0 for i in range(0, len(choices))]
-            print(shares)
-            weights = [(shares[i]*lastPrices[i])/self.amount for i in range(0, len(choices))]
-            sumWeights = sum(weights)
-            print(sumWeights)
-            if (1 - confidence) < sumWeights <= 1:
-                print("ok!")
-                break
-        self.weights = weights
-        self.shares = shares
-    
-
-    def RandomWeightsfirst(self, confidence=0.01, numberOfAssets=38):
-        lastPrices = self.listOfAssets.LastPrices()
-        choices = self.ChooseAssets(numberOfAssets)
-        maxPercentage = 0.2 if len(choices) > 5 else 0.5
-        #print(choices)
-        print("First")
-        while True:
-            shares = [0 for i in range(0, len(self.listOfAssets.listAssets))]
-            amountToBeFilled = self.amount
-            for i in range(0, len(choices)):
-                maxShares = int(amountToBeFilled / lastPrices[choices[i]]) if amountToBeFilled \
-                    < maxPercentage * self.amount else int(self.amount * maxPercentage / lastPrices[choices[i]])
-                if maxShares == 0:
-                    continue
-                shares[choices[i]] = rd.randint(1, maxShares)
-                amountToBeFilled -= shares[choices[i]] * lastPrices[choices[i]]
-
-            print(shares)
-            sum = 0
-            for i in range(0, len(shares)):
-                sum += shares[i]*lastPrices[i]
-
-            if (1-confidence) < sum/self.amount <= 1:
-                break
-
-            #weights = [(shares[i]*lastPrices[i])/self.amount for i in range(0, len(choices))]
-            #sumWeights = sum(weights)
-            #print(sumWeights)
-            #if (1 - confidence) < sumWeights <= 1:
-            #    print("ok!")
-            #    break
-        #self.weights = weights
-        #self.shares = shares
-        self.weights = [shares[i]*lastPrices[i]/self.amount for i in range(0, len(shares))]
-        print(shares)
-        print(str(sum/self.amount))
-
-    def RandomWeightsTest(self, confidence=0.01, numberOfAssets=38):
-        #lastPrices = self.listOfAssets.LastPrices()
-        choices = self.ChooseAssets(numberOfAssets)
-        maxPercentage = 0.125 if len(choices) > 5 else 0.5
-        minPercentage = 0.001
-        while True:
-            weights = list()
-            weightsCum = 0
-            for i in range(0, numberOfAssets):
-                if (1 - weightsCum) < minPercentage:
-                    break
-                weights.append((rd.randint(int(minPercentage*1000), int(min(1-weightsCum, maxPercentage)*1000)))/1000)
-                #print(rd.randint(int(minPercentage*10000), int(min(float(1-weightsCum), maxPercentage)*10000))/10000)
-                #print(int(minPercentage*10000))
-                weightsCum += weights[i]
-            print(weightsCum)
-            print(len(weights))
-            if 1 - confidence < weightsCum <= 1 and len(weights) == numberOfAssets:
-                break
-        self.weights = weights
-        print(weights)
-    
-    def RandomWeightsTer(self, confidence=0.01, numberOfAssets=20):
-        #lastPrices = self.listOfAssets.LastPrices()
-        choices = self.ChooseAssets(numberOfAssets)
-        maxPercentage = 0.4 if len(choices) > 5 else 0.5
-        minPercentage = 0.001
-        while True:
-
-            #weights = [rd.randint(minPercentage*10000, maxPercentage*10000)/10000 for i in range(0, numberOfAssets)]
-            weights = [rd.randint(1, 100000) / 100000 for i in range(0, numberOfAssets)]
-            #print(sum(weights))
-            weights = [i / sum(weights) for i in weights]
-            if 1 - confidence < sum(weights) <= 1:
-                break
-
-        self.weights = weights
-        print(sum(weights))
-        print(max(weights))
-        print(min(weights))
-        #print(weights)
-    """
-
-    """
-    def RandomWeightsBis(self, numberOfAssets=38):
-        choices = self.ChooseAssets(numberOfAssets)
-        #maxPercentage = 0.4 if len(choices) > 5 else 0.5
-        #minPercentage = 0.001
-        weights = [rd.randint(1000, 100000) / 100000 for i in range(0, numberOfAssets)]
-        weights = [i / sum(weights) for i in weights]
-        self.weights = [0 for i in range(0,len(self.listOfAssets.listAssets))]
-        index = 0
-        for i in range(0, len(self.weights)):
-            if i in choices:
-                self.weights[i] = weights[index]
-                index += 1
-    """
 
     def RandomWeights(self):
         choices = self.ChooseAssets()
--- CodeWithDB/VarCov.py
+++ CodeWithDB/VarCov.py
@@ -1,33 +1,9 @@
 # ------------- Var_Cov Class ------------- #
-# Packages
-
-# Libraries
-import numpy as np
 
 
 class VarCov:
-    '''Definition de la matrice de covariance direct par la fonction cov'''
+    """
+    Definition de la matrice de covariance direct par la fonction cov
+    """
     def __init__(self, returns):
         self.matrix = returns.astype(float).cov()
-    
-    '''Recuperation de la Variance de chaque assets à partir de la Mat de VARCOV
-     (donc recupération de la diagonal en faite) sous forme de tableau'''
-    def getVol(self):
-        """
-        Returns the volatility of each asset
-        """
-        list_of_var = []
-        for i in range(0, len(self.matrixVarCov[0])):
-            list_of_var.append((self.matrixVarCov[i][i])**0.5)
-        return list_of_var
-
-    '''Renvoie la matrice de VARCOV d'une serie stat "data" sous forme de tableau Numpy
-    Ne concerne pas du tout self donc : pourquoi est-ce dans cette classe : la mette dans Functions ???'''
-    def Compute_var_cov(self, data):
-        data_arr = np.array(data)
-        cov_matrix = np.cov(data_arr, rowvar=False)
-        return cov_matrix
-
-    '''Surcharge du Print()'''
-    def __str__(self):
-        return str(self.matrixVarCov)
