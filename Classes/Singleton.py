2f9ca7c0723c9d7fe79645b3ddbaf626afb192aa
AntoineMf <itsezax@gmail.com>
2022-07-20 11:07:38 +0800

    fixing struct

--- Classes/Singleton.py
+++
@@ -1,7 +0,0 @@
-class Singleton(type):
-    __instances = {}
-
-    def __call__(class__, *args, **kwargs):
-        if class__ not in class__.__instances:
-            class__.__instances[class__] = super(Singleton, class__).__call__(*args, **kwargs)
-        return class__.__instances[class__]
